fe9e421b488bc0b4c138f4d139a33e68270c8122
Robert F. Dickerson <rfdickerson@gmail.com>
2016-11-14 14:55:14 -0600

    Update schema.sql

--- Database/schema.sql
+++ Database/schema.sql
@@ -1,6 +1,6 @@
 DROP TABLE todos;
 
-CREATE TABLE todos(tid INTEGER PRIMARY KEY, 
+CREATE TABLE todos(tid SERIAL PRIMARY KEY, 
                     user_id TEXT NOT NULL, 
                     title TEXT NOT NULL, 
                     completed boolean NOT NULL, 
